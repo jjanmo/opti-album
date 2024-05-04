4dbf42a82de050d425326fdd93572fd0e9de38c1
jjanmo <jjanmo07@hanmail.net>
2024-05-04 23:15:26 +0900

    refactor: upload 라우터 분리

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,11 @@
-import express, { Request, Response } from 'express'
-import multer from 'multer'
+import express from 'express'
 import 'dotenv/config'
-import { storage } from './middlewares/image'
+import uploadRouter from './routes/upload'
 
 const app = express()
-const upload = multer({ storage })
 const port = process.env.PORT || 4000
 
-app.post('/upload', upload.single('image'), (req: Request, res: Response) => {
-  console.log('uploaded', req.body, req.file)
-  res.send('File uploaded successfully')
-})
+app.use('/', uploadRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
