97a6dd089071c7abd9a75278fde119928207dad9
jjanmo <jjanmo07@hanmail.net>
2024-05-04 15:44:45 +0900

    feat: upload 라우터 추가 및 multer 설정

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,16 @@
-import express from "express";
-import "dotenv/config";
+import express, { Request, Response } from 'express'
+import multer from 'multer'
+import 'dotenv/config'
 
-const app = express();
-const port = process.env.PORT || 4000;
+const app = express()
+const upload = multer({ dest: 'uploads/' })
+const port = process.env.PORT || 4000
+
+app.post('/upload', upload.single('image'), (req: Request, res: Response) => {
+  console.log('uploaded', req.body, req.file)
+  res.send('File uploaded successfully')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
