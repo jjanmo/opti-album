7143987294aa46e0ad5dce49244726f816a6afa8
jjanmo <jjanmo07@hanmail.net>
2024-05-04 21:09:50 +0900

    feat: 유일한 이미지 이름 파일 생성 로직 추가

--- server/middlewares/image.ts
+++ server/middlewares/image.ts
@@ -1,11 +1,14 @@
 import multer from 'multer'
+import { randomUUID } from 'crypto'
+import mime from 'mime-types'
 
 export const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    console.log('file', file)
-    cb(null, file.originalname)
+    const filename = randomUUID()
+    const ext = mime.extension(file.mimetype)
+    cb(null, `${filename}.${ext}`)
   },
 })
