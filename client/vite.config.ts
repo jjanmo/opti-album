0d191f2d9a3e39dad29e5ec79a5730e96cb2b887
jjanmo <jjanmo07@hanmail.net>
2024-05-05 17:08:04 +0900

    feat: 좀더 간단하 cors 설정 추가

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,11 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      '/upload': 'http://localhost:4000',
     },
   },
 })
