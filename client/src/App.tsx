04dfd80c23d2deb2503e2bc0e4b66bfac5335616
jjanmo <jjanmo07@hanmail.net>
2024-05-05 12:19:41 +0900

    feat: install tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Opti Album</h1>
+    </div>
+  )
 }
 
 export default App
