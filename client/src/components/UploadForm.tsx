07e7f8f385352313119b9f3b6a0c869412f3a8eb
jjanmo <jjanmo07@hanmail.net>
2024-05-08 07:33:07 +0900

    fix: 프로그레스바 로딩 상태 애니메이션을 위한 수정

--- client/src/components/UploadForm.tsx
+++ client/src/components/UploadForm.tsx
@@ -4,19 +4,17 @@ import {
   usePostUploadImageMutation,
 } from '@/queries/useUploadMutation'
 import { AxiosProgressEvent } from 'axios'
+import { toast } from 'react-toastify'
 
 const PROGRESS_BAR_WIDTH = 400
 
 const UploadForm = () => {
   const [file, setFile] = useState<File | null>(null)
   const [barWidth, setBarWidth] = useState(0)
+  const [loading, setLoading] = useState(false) // progress bar를 보여주기 위한 state
   const thumbnailRef = useRef<HTMLImageElement>(null)
 
-  const {
-    mutate: uploadImageMutate,
-    isPending,
-    isSuccess,
-  } = usePostUploadImageMutation()
+  const { mutate: uploadImageMutate } = usePostUploadImageMutation()
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
@@ -34,6 +32,8 @@ const UploadForm = () => {
     const formData = new FormData()
     formData.append('image', file)
 
+    setLoading(true)
+
     const payload: PostImagePayload = {
       url: '/upload',
       data: formData,
@@ -42,11 +42,30 @@ const UploadForm = () => {
         onUploadProgress: (e: AxiosProgressEvent) => {
           if (!e.total) setBarWidth(0)
           else
-            setBarWidth(Math.floor((e.loaded / e.total) * PROGRESS_BAR_WIDTH))
+            setBarWidth(Math.floor((e.loaded / e.total!) * PROGRESS_BAR_WIDTH))
         },
       },
     }
-    uploadImageMutate(payload)
+    uploadImageMutate(payload, {
+      onSuccess: () => {
+        new Promise<void>((resolve) => {
+          setTimeout(() => {
+            setFile(null)
+            setBarWidth(0)
+            setLoading(false)
+            if (thumbnailRef.current) {
+              URL.revokeObjectURL(thumbnailRef.current?.src || '')
+            }
+            resolve()
+          }, 200)
+        }).then(() => {
+          toast.success('Image upload successful 🎉')
+        })
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      },
+    })
   }
 
   useEffect(() => {
@@ -54,15 +73,6 @@ const UploadForm = () => {
       thumbnailRef.current.src = URL.createObjectURL(file)
     }
   }, [file])
-
-  useEffect(() => {
-    if (isSuccess) {
-      setFile(null)
-      setBarWidth(0)
-      if (thumbnailRef.current)
-        URL.revokeObjectURL(thumbnailRef.current?.src || '')
-    }
-  }, [isSuccess])
 
   return (
     <form className="flex flex-col my-3" onSubmit={handleSubmit}>
@@ -89,10 +99,10 @@ const UploadForm = () => {
         type="file"
         accept="image/*"
         className="hidden"
-        disabled={isPending}
+        disabled={loading}
         onChange={handleChange}
       />
-      {isPending ? (
+      {loading ? (
         <div className={styles.progressBar}>
           <div className={styles.filled} style={{ width: `${barWidth}px` }} />
           <div className={styles.filledText}>Uploading...</div>
@@ -110,9 +120,9 @@ export default UploadForm
 
 const styles = {
   label:
-    'w-full h-[225px] flex flex-col justify-center items-center bg-slate-100 rounded-sm cursor-pointer border-2 border-dashed border-indigo-300',
+    'group w-full h-[225px] flex flex-col justify-center items-center bg-slate-100 rounded-sm cursor-pointer border-2 border-dashed border-indigo-300',
   fileName:
-    'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-2 px-4 text-sm text-center text-white bg-slate-500 opacity-70 rounded-md hover:opacity-100 transition-opacity duration-300 ease-in-out z-10',
+    'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-2 px-4 text-sm text-center text-white bg-slate-500 opacity-0 rounded-md group-hover:opacity-80 transition-opacity duration-500 ease-in-out z-10',
   overlay: 'absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.2)]',
   thumbnail: 'w-full h-full object-contain',
   choose: 'py-3 px-6 mb-3 text-white bg-indigo-500 font-semibold rounded-md',
