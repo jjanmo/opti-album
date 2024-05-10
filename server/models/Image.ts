98351afb52662995441122fc0152178e982da875
jjanmo <jjanmo07@hanmail.net>
2024-05-10 23:51:20 +0900

    fix: 스키마 네이밍 및 옵션 설정

--- server/models/Image.ts
+++ server/models/Image.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
 const { Schema } = mongoose
 
-export const imageSchema = new Schema({
-  id: { type: String, required: true },
-  originalFileName: { type: String, required: true },
-  size: { type: Number, required: true },
-  type: { type: String, required: true },
-})
+export const ImageSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    originalName: { type: String, required: true },
+    size: { type: Number, required: true },
+    type: { type: String, required: true },
+  },
+  { timestamps: true }
+)
