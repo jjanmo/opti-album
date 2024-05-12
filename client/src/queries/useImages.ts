67cd10be472538a944e8adc3902e23575f9dcf86
jjanmo <jjanmo07@hanmail.net>
2024-05-12 22:44:28 +0900

    feat: response 타입 인터페이스 추가

--- client/src/queries/useImages.ts
+++ client/src/queries/useImages.ts
@@ -1,8 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { Response } from './types'
+
+export interface Image {
+  _id: string
+  key: string
+  type: string
+  originalName: string
+  size: number
+  createdAt: string
+  updatedAt: string
+}
 
 export const getImages = async () => {
-  const response = await axios.get('/images').then((res) => res.data)
+  const response = await axios
+    .get<Response<Image[]>>('/images')
+    .then((res) => res.data)
   return response.data
 }
 
