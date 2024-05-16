502ec7563b6cc69c13da6f1cd9b38959229447e1
jjanmo <jjanmo07@hanmail.net>
2024-05-16 22:42:15 +0900

    feat: 회원가입 로직 추가

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,17 +1,23 @@
 import express from 'express'
+import UserModel from '../models/User'
 
 const router = express.Router()
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   res.status(201).json({
     status: 'success',
   })
 })
-router.post('/signup', (req, res) => {
+
+router.post('/signup', async (req, res) => {
+  const user = new UserModel(req.body)
+  await user.save()
+
   res.status(201).json({
     status: 'success',
   })
 })
+
 router.post('/logout', (req, res) => {
   res.status(201).json({
     status: 'success',
