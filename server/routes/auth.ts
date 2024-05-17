412e9bc9a46a860c1bfe63a6c9d639f5eadfc667
jjanmo <jjanmo07@hanmail.net>
2024-05-17 23:20:40 +0900

    feat: 회원가입 암호화 로직 구현

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import UserModel from '../models/User'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
@@ -21,12 +22,24 @@ router.post('/login', async (req, res) => {
 })
 
 router.post('/signup', async (req, res) => {
-  const user = new UserModel(req.body)
-  await user.save()
+  const { nickname, password, email } = req.body
 
-  res.status(201).json({
-    status: 'success',
-  })
+  try {
+    bcrypt.hash(password, 10, async (err, hash) => {
+      const user = new UserModel({
+        nickname,
+        password: hash,
+        email,
+      })
+      await user.save()
+
+      return res.status(201).json({
+        status: 'success',
+      })
+    })
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 router.post('/logout', (req, res) => {
